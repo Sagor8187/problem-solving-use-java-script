9badfdd038bc9d72d08af333a406af782a19a926
Sagor8187 <sdsagor8187@gmail.com>
2026-02-07 12:26:28 +0600

    now i solve problem 4

--- problem4.js
+++ problem4.js
@@ -1,3 +1,27 @@
 function gonoVote(array) {
-          //write your code here
+      
+        if (Array.isArray(array)){
+            let ha = 0;
+            let na = 0;
+            for(let vot of array){
+                if(vot === "ha"){
+                    ha+=1;
+
+                }else{
+                    na+=1;
+                }
+            }
+            if(ha > na){
+                return true
+            }else if(ha < na){
+                return false
+            }else {
+                return "equal"
+            }
+        }
+        else{
+            return "Invalid"
+        }
 }
+
+console.log(gonoVote(["ha", "na", "ha", "na"]))
